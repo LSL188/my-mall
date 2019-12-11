78f5965ba5234b2f17d67b397b50c7d1b59f588a
刘素莉 <1114317678@qq.com>
2019-12-11 17:34:07 +0800

    tab-control finish

---
+++ src/components/content/tabcontrol/TabControl.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="tab-control">
+    <div
+      class="tab-control-item"
+      v-for="(item, i) in titles"
+      :key="i"
+      @click="itemClick(i)"
+      :class="{ active: currentIndex === i }"
+    >
+      <span>{{ item }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentIndex: 0
+    };
+  },
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+.tab-control {
+  background-color: #fff;
+  display: flex;
+  text-align: center;
+  height: 40px;
+  line-height: 40px;
+  .tab-control-item {
+    flex: 1;
+    color: #666;
+    span {
+       padding: 0 10px 8px 10px;
+    }
+  }
+}
+
+.active {
+  span {
+    color: @navColor;
+    border-bottom: 2px solid @navColor;
+  }
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,14 +6,64 @@
     <home-swiper :banners="banner"></home-swiper>
     <home-recommend :recommends="recommend"></home-recommend>
     <home-week></home-week>
+    <tab-control :titles="['流行', '新款', '精选']" class="home-tab-control"></tab-control>
+
+    <ul>
+      <li>12</li>
+      <li>22</li>
+      <li>32</li>
+      <li>42</li>
+      <li>52</li>
+      <li>42</li>
+      <li>52</li>
+      <li>62</li>
+      <li>72</li>
+      <li>82</li>
+      <li>52</li>
+      <li>42</li>
+      <li>52</li>
+      <li>62</li>
+      <li>72</li>
+      <li>82</li>
+      <li>52</li>
+      <li>42</li>
+      <li>52</li>
+      <li>62</li>
+      <li>72</li>
+      <li>82</li>
+      <li>52</li>
+      <li>42</li>
+      <li>52</li>
+      <li>62</li>
+      <li>72</li>
+      <li>82</li>
+      <li>92</li>
+      <li>102</li>
+      <li>62</li>
+      <li>72</li>
+      <li>82</li>
+      <li>92</li>
+      <li>102</li>
+      <li>112</li>
+      <li>122</li>
+      <li>132</li>
+      <li>142</li>
+      <li>152</li>
+      <li>162</li>
+      <li>172</li>
+      <li>182</li>
+      <li>192</li>
+      <li>202</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
-import HomeSwiper from './childCpn/HomeSwiper'
-import HomeRecommend from './childCpn/HomeRecommend'
-import HomeWeek from './childCpn/HomeWeek'
+import HomeSwiper from "./childCpn/HomeSwiper";
+import HomeRecommend from "./childCpn/HomeRecommend";
+import HomeWeek from "./childCpn/HomeWeek";
+import TabControl from "components/content/tabcontrol/TabControl";
 
 import { getHomeData } from "network/home";
 export default {
@@ -28,7 +78,8 @@ export default {
     NavBar,
     HomeSwiper,
     HomeRecommend,
-    HomeWeek
+    HomeWeek,
+    TabControl
   },
   created() {
     this._getHomeData();
@@ -50,13 +101,17 @@ export default {
 
 <style scoped lang="less">
 .home {
-    padding-top: 44px;
-    .home-nav {
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        z-index: 2;
-    }
+  padding-top: 44px;
+  .home-nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 2;
+  }
+  .home-tab-control {
+      position: sticky;
+      top: 44px;
+  }
 }
 </style>
